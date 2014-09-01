9488b0886ec372fb10f192533ad701b48a65823f
Chris Roth <chris@cjroth.com>
2014-09-01 15:02:25 -0400

    stop tracking dist

--- dist/scripts/default/sidebar-controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('lifetracker').controller('DefaultSidebarController', function($rootScope, $scope, store) {
-  $scope.select = function(variable) {
-    return variable.selected = !variable.selected;
-  };
-});
--- dist/scripts/nav-controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('lifetracker').controller('NavController', function($scope, $state) {
-  $scope.$state = $state;
-  $('.datepicker').datepicker({
-    inputs: $('.range-start, .range-end')
-  });
-});
